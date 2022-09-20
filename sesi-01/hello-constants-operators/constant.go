34a8600a69fc80a8b95a115bcc82110342ea4b00
rennyks <rennnyksalainti16@gmail.com>
2022-09-20 19:53:58 +0800

    update

--- sesi-01/hello-constants-operators/constant.go
+++
@@ -1,8 +0,0 @@
-package main
-
-import "fmt"
-
-func main() {
-	const full_name string = "Renny Salainti"
-	fmt.Printf("Hello %s", full_name)
-}
--- sesi-01/hello-data-types/hello_data_types.go
+++ sesi-01/hello-data-types/hello_data_types.go
@@ -2,11 +2,24 @@ package main
 
 import "fmt"
 
-func main(){
-
-	var first = 89
-	var second = -12
+func main() {
+	//number (integer) tipe data Integer
+	var first uint8 = 89
+	var second int8 = -12
 
 	fmt.Printf("tipe data first : %T\n", first)
 	fmt.Printf("bilangan second : %T\n", second)
+	fmt.Println("===============================")
+
+	//belajar number(float)
+	var decimalNumber float32 = 3.63
+
+	fmt.Printf("decimal Number : %f\n", decimalNumber)
+	fmt.Printf("decimal Number : %.3f\n", decimalNumber)
+	fmt.Println("===============================")
+
+	//belajar Bool
+	var condition bool = true
+	fmt.Printf("is it premitted? %t \n", condition)
+	fmt.Println("===============================")	
 }
