fbbc719ff1998b389300449071bdc7bc5260aa5c
rennyks <rennnyksalainti16@gmail.com>
2022-09-22 00:20:19 +0800

    update

--- sesi-02/hi-loopings/hi_loopings.go
+++ sesi-02/hi-loopings/hi_loopings.go
@@ -5,18 +5,21 @@ import (
 )
 
 func main() {
+
+	//first way of looping
 	// for i := 0; i < 3; i++{
 	// 	fmt.Println("Angka", i)
 
 	// }
-
+	//second way of looping
 	//var i = 1
-
+	
 	// for i <= 10{
 	// 	fmt.Println("angka", i)
 	// 	i++
 	// }
-
+	
+	//third way of looping
 	// for{
 	// 	fmt.Println("Angka", i)
 	// 	i++
@@ -25,6 +28,7 @@ func main() {
 	// 	}
 	// }
 
+	//break and continue keywords
 	// for i := 1; i <= 10; i++{
 	// 	if i%2 == 1{
 	// 		continue
@@ -36,13 +40,17 @@ func main() {
 
 	// 	fmt.Println("Angka", i)
 	// }
-		
+
+	
+	//nested looping	
 	// for i := 0; i < 5; i++ {
 	// for j := i; j < 5; j++ {
 	// fmt.Print(j, " ")
 	// 	}
 	// fmt.Println()
 	// }
+
+	// label
 	// outerLoop:
 	// for i := 0; i < 3; i++ {
 	// 	fmt.Println("Perulangan ke - ", i+1)
@@ -55,7 +63,7 @@ func main() {
 	// 	}
 	// 	fmt.Print("\n")
 	// }
-
+	
 	// var numbers [4]int
 	// numbers = [4]int{1, 2, 3, 4}
 	
