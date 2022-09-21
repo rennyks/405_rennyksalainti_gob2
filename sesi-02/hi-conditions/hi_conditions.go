78e3dcd74c43d7f4a17fddf323a0bb089db14318
rennyks <rennnyksalainti16@gmail.com>
2022-09-21 23:58:53 +0800

    update

--- sesi-02/hi-conditions/hi_conditions.go
+++ sesi-02/hi-conditions/hi_conditions.go
@@ -1,5 +1,7 @@
 package main
 
+import "fmt"
+
 func main() {
 	// temporary variabel
 	// var currentYear = 2021
@@ -11,7 +13,7 @@ func main() {
 	// }
 
 	//switch
-	//
+	var score = 1
 	// switch score {
 	// case 8:
 	// fmt.Println("Perfect")
@@ -33,5 +35,41 @@ func main() {
 	// 	fmt.Println("you need to learn more")
 	// }
 	// }
+
+	//switch ( falthrough keyword)
+	// switch{
+	// case score == 8:
+	// 	fmt.Println("perfect")
+	// case (score < 8) && (score > 3):
+	// 	fmt.Println("not bad")
+	// fallthrough
+	// case score < 5:
+	// 	fmt.Println("it is ok, but please study harder")
+	// default:{
+	// 	fmt.Println("study harder")
+	// 	fmt.Println("you don't have a good score yet")
+	// }
+	// }
+
+	//nested conditions
+	if score > 7{
+		switch score {
+		case 10:
+			fmt.Println("perfect!")
+		default:
+			fmt.Println("nice!")
+		}
+	}else{
+		if score == 5{
+			fmt.Println("not bad")
+		} else if score == 3 {
+			fmt.Println("keep trying")
+		}else{
+			fmt.Println("you can do it")
+			if score == 0{
+				fmt.Println("TRY HARDER!")
+			}
+		}
+	}
 }
 
