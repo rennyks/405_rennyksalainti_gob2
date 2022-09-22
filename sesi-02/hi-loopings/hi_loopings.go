af28a1405c221cfefef1db19f57200c4023215ad
rennyks <rennnyksalainti16@gmail.com>
2022-09-23 00:18:38 +0800

    update

--- sesi-02/hi-loopings/hi_loopings.go
+++ sesi-02/hi-loopings/hi_loopings.go
@@ -7,62 +7,28 @@ import (
 func main() {
 
 	//first way of looping
-	// for i := 0; i < 3; i++{
-	// 	fmt.Println("Angka", i)
+	for i := 0; i < 3; i++{
+		fmt.Println("Angka", i)
 
-	// }
+	}
 	//second way of looping
-	//var i = 1
+	var i = 1
 	
-	// for i <= 10{
-	// 	fmt.Println("angka", i)
-	// 	i++
-	// }
+	for i <= 10{
+		fmt.Println("angka", i)
+		i++
+	}
 	
 	//third way of looping
-	// for{
-	// 	fmt.Println("Angka", i)
-	// 	i++
-	// 	if i == 3 {
-	// 		break
-	// 	}
-	// }
-
-	//break and continue keywords
-	// for i := 1; i <= 10; i++{
-	// 	if i%2 == 1{
-	// 		continue
-	// 	}
-
-	// 	if i > 8 {
-	// 		break
-	// 	}
-
-	// 	fmt.Println("Angka", i)
-	// }
+	for{
+		fmt.Println("Angka", i)
+		i++
+		if i == 3 {
+			break
+		}
+	}
 
 	
-	//nested looping	
-	// for i := 0; i < 5; i++ {
-	// for j := i; j < 5; j++ {
-	// fmt.Print(j, " ")
-	// 	}
-	// fmt.Println()
-	// }
-
-	//label
-	outerLoop:
-	for i := 0; i < 3; i++ {
-		fmt.Println("Perulangan ke - ", i+1)
-		for j := 0; j < 3; j++ {
-			if i == 2 {
-				break outerLoop
-			}
-
-			fmt.Print(j, " ")
-		}
-		fmt.Print("\n")
-	}
 	
 	
 }
--- sesi-02/hi-strings-in-depth/hi_02.go
+++ sesi-02/hi-strings-in-depth/hi_02.go
@@ -2,20 +2,26 @@ package main
 
 import (
 	"fmt"
-	"unicode/utf8"
 )
 
 func main(){
 	// looping over string (rune-by-rune)
-	str1 := "makan"
-	str2 := "mancai"
+	// str1 := "makan"
+	// str2 := "mancai"
 
 	// fmt.Printf("str1 byte lenght => %d\n", len(str1))
 	// fmt.Printf("str2 byte lenght => %d\n", len(str2))
 
 	//menggunakan function RuneCountlnString dari package utf8
-	fmt.Printf("str1 character length => %d\n", utf8.RuneCountInString(str1))
-	fmt.Printf("str2 character length => %d\n", utf8.RuneCountInString(str2))
+	// fmt.Printf("str1 character length => %d\n", utf8.RuneCountInString(str1))
+	// fmt.Printf("str2 character length => %d\n", utf8.RuneCountInString(str2))
+
+	//menggunakan range loop
+	str := "manca"
+
+	for i, s := range str {
+	fmt.Printf("index => %d, string => %s\n", i, string(s))
+	}
 
 	
 }
