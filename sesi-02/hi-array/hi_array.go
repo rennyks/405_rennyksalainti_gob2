a9b903d4d5a796e49ef0935babfbc2cb43e29c0d
rennyks <rennnyksalainti16@gmail.com>
2022-09-22 12:58:17 +0800

    update

--- sesi-02/hi-array/hi_array.go
+++ sesi-02/hi-array/hi_array.go
@@ -3,14 +3,13 @@ package main
 import "fmt"
 
 func main() {
-	// var numbers [4]int
-	// numbers = [4]int{1, 2, 3, 4}
+	var numbers [4]int
+	numbers = [4]int{1, 2, 3, 4}
 
-	// var strings = [3]string{"Airell", "Nanda", "Mailo"}
+	var strings = [3]string{"Airell", "Nanda", "Mailo"}
 
-	// fmt.Printf("%#v\n", numbers)
-	// fmt.Printf("%#v\n", strings) 
-
+	fmt.Printf("%#v\n", numbers)
+	fmt.Printf("%#v\n", strings) 
 
 	// var fruits = [3]string{"apel", "pisang", "mangga"}
 	// fruits[0] = "apple"
@@ -19,7 +18,8 @@ func main() {
 
 	// fmt.Printf("%#v\n", fruits)
 
-	// var fruits = [3]string{"apel", "pisang", "mango"}
+	var fruits = [3]string{"apel", "pisang", "mango"}
+	
 	// //01
 	// for i, v := range fruits {
 	// 	fmt.Printf("index : %d, value: %s\n", i, v)
@@ -27,16 +27,7 @@ func main() {
 	// fmt.Println(strings.Repeat("#", 25))
 
 	// //02
-	// for i := 0; i < len(fruits); i++{
-	// 	fmt.Printf("index: %d, value: %s\n", i, fruits[i])
-	// }
-
-	balances := [2][3]int{{5, 6, 7},{8, 9, 10}}
-
-	for _, arr := range balances {
-		for _, value := range arr {
-			fmt.Printf("%d ", value)
-		}
-		fmt.Println()
+	for i := 0; i < len(fruits); i++{
+		fmt.Printf("index: %d, value: %s\n", i, fruits[i])
 	}
 }
