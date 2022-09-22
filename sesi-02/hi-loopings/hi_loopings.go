801810b1266fbf95d0b595431622a164ac45e9bc
rennyks <rennnyksalainti16@gmail.com>
2022-09-22 14:26:03 +0800

    update

--- sesi-02/hi-loopings/hi_loopings.go
+++ sesi-02/hi-loopings/hi_loopings.go
@@ -50,53 +50,20 @@ func main() {
 	// fmt.Println()
 	// }
 
-	// label
-	// outerLoop:
-	// for i := 0; i < 3; i++ {
-	// 	fmt.Println("Perulangan ke - ", i+1)
-	// 	for j := 0; j < 3; j++ {
-	// 		if i == 2 {
-	// 			break outerLoop
-	// 		}
+	//label
+	outerLoop:
+	for i := 0; i < 3; i++ {
+		fmt.Println("Perulangan ke - ", i+1)
+		for j := 0; j < 3; j++ {
+			if i == 2 {
+				break outerLoop
+			}
 
-	// 		fmt.Print(j, " ")
-	// 	}
-	// 	fmt.Print("\n")
-	// }
-	
-	// var numbers [4]int
-	// numbers = [4]int{1, 2, 3, 4}
-	
-	// var strings = [3] string {"arell", "nanda", "mailo"}
-	// fmt.Printf("%#v\n", numbers)
-	// fmt.Printf("%#v\n", strings)
-
-	// var fruits = [3]string{"apel", "pisang", "mangga"}
-	// fruits[0] = "apple"
-	// fruits[1] = "banana"
-	// fruits[2] = "manggo"
-
-	// fmt.Printf("%#v\n", fruits)
-
-	// var fruits = [3]string{"apel", "pisang", "mango"}
-	// //01
-	// for i, v := range fruits {
-	// 	fmt.Printf("index : %d, value: %s\n", i, v)
-	// }
-	// fmt.Println(strings.Repeat("#", 25))
-
-	// //02
-	// for i := 0; i < len(fruits); i++{
-	// 	fmt.Printf("index: %d, value: %s\n", i, fruits[i])
-	// }
-
-	balances := [2][3]int{{5, 6, 7},{8, 9, 10}}
-
-	for _, arr := range balances {
-		for _, value := range arr {
-			fmt.Printf("%d ", value)
+			fmt.Print(j, " ")
 		}
-		fmt.Println()
+		fmt.Print("\n")
 	}
+	
+	
 }
 
--- sesi-02/hi-slice/hi_slice.go
+++ sesi-02/hi-slice/hi_slice.go
@@ -1,12 +1,44 @@
 package main
 
+import "fmt"
+
 func main() {
-	var fruits = []string{"apple", "banana", "mango"}
-	_ = fruits
+	//make function
+	// var fruits = []string{"apple", "banana", "mango"}
+	// _ = fruits
 
 	// var fruits = make([]string, 3)
-
 	// _= fruits
+	// fmt.Printf("%#v", fruits)
+
+	//sppend function
+	// var fruits = make([]string, 3)
+	// _ = fruits
+
+	// fruits[0] = "apple"
+	// fruits[1] = "banana"
+	// fruits[2] = "mango"
 
 	// fmt.Printf("%#v", fruits)
+
+	// fruits = append(fruits, "apple", "banana", "mango")
+	// fmt.Printf("%#v", fruits)
+
+	//append function with ellipsis
+	// var fruits1 = []string{"apple", "banana", "mango"}
+	// var fruits2 = []string{"durian", "pineapple", "starfruit"}
+
+	// fruits1 = append(fruits1, fruits2...)
+	// fmt.Printf("%#v", fruits1)
+
+	//copy function
+	var fruits1 = []string{"apple", "banana", "mango"}
+	var fruits2 = []string{"durian", "pineapple", "starfruit"}
+
+	nn := copy(fruits1, fruits2)
+
+	fmt.Println("Fruits1 =>", fruits1)
+	fmt.Println("Fruits2 =>", fruits2)
+	fmt.Println("Copied elements => ", nn)
+
 }
