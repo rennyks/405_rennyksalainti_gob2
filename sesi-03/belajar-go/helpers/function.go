2193cdef229f6ad7ec4675192ac92f06989eb922
rennyks <rennnyksalainti16@gmail.com>
2022-09-24 14:43:02 +0800

    update

---
+++ sesi-03/belajar-go/helpers/function.go
@@ -0,0 +1,12 @@
+package helpers
+
+import "fmt"
+
+func Greet() {
+
+	fmt.Println("hallo dari function Greet")
+}
+
+func greet() {
+	fmt.Println("hallo dari function greet")
+}
---
+++ sesi-03/belajar-go/main.go
@@ -0,0 +1,11 @@
+package main
+
+import "belajar-go/helpers"
+
+func main() {
+	helpers.Greet()
+	
+	var person = helpers.Person{}
+
+	person.Invokegreet()
+}
