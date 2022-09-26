6ed86963af7b84f2a3ce55428e2f11fa8e1ba5c0
rennyks <rennnyksalainti16@gmail.com>
2022-09-26 22:20:29 +0800

    update

--- sesi-04/interface/interface.go
+++ sesi-04/interface/interface.go
@@ -39,24 +39,37 @@ func print(t string, s shape) {
 	fmt.Printf("%s perimeter %v\n", t, s.perimeter())
 }
 
+func (c circle) volume() float64{
+return 4 / 3 * math.Pi * math.Pow(c.radius, 3)
+}	
+
 func main() {
 	var c1 shape = circle{radius: 5}
 	var r1 shape = rectangle{width: 3, height: 2}
 
-	fmt.Printf("Type of c1: %T\n", c1)
-	fmt.Printf("Type of r1: %T\n", r1)
-	fmt.Println("============================")
+	// fmt.Printf("Type of c1: %T\n", c1)
+	// fmt.Printf("Type of r1: %T\n", r1)
+	// fmt.Println("============================")
 
-	fmt.Println("Circle area", c1.area())
-	fmt.Println("Circle perimeter", c1.perimeter())
+	// fmt.Println("Circle area", c1.area())
+	// fmt.Println("Circle perimeter", c1.perimeter())
 
-	fmt.Println("Rectangle area", r1.area())
-	fmt.Println("Regtangle perimeter", r1.perimeter())
-	fmt.Println("=================================")
+	// fmt.Println("Rectangle area", r1.area())
+	// fmt.Println("Regtangle perimeter", r1.perimeter())
+	// fmt.Println("=================================")
 
 	print("Rectangle", c1)
 	print("Circle", r1)
 	fmt.Println("=================================")
+
+	// c1.(circle).volume()
+
+	value, ok := c1.(circle)
+
+	if ok == true {
+		fmt.Printf("circle value: %+v\n", value)
+		fmt.Printf("circle volume %f", value.volume())
+	}
 
 
 }
