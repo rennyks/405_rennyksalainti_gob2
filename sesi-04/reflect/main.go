156b65932f5da4bef1e15a739de53fb0fbb42d8b
rennyks <rennnyksalainti16@gmail.com>
2022-09-26 23:45:28 +0800

    update

--- sesi-04/reflect/main.go
+++ sesi-04/reflect/main.go
@@ -9,11 +9,10 @@ func main() {
 	var number = 23 //
 	var reflectValue = reflect.ValueOf(number)
 
-	fmt.Printf("reflect value : %+V\n", number)
-	fmt.Printf("tipe value : %+V\n", reflect.Value.Type())
+	fmt.Printf("tipe value : %+v\n", reflectValue.Type())
 
-
-	fmt.Printf("nilai variabel: %+V\n", reflectValue.Interface())
-
+	if reflectValue.Kind() == reflect.Int {
+	fmt.Printf("nilai variabel: %+v\n", reflectValue.Interface())
 	
+	}	
 }
