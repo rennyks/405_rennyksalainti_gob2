0dc6411dff42719a701c5502ea518a00b3b5115a
rennyks <rennnyksalainti16@gmail.com>
2022-10-04 09:45:56 +0800

    update gorm

---
+++ sesi-07/GORM/main.go
@@ -0,0 +1,9 @@
+package main
+
+import (
+	"GORM/database"
+)
+
+func main() {
+	database.StartDB()
+}
---
+++ sesi-07/GORM/models/product.go
@@ -0,0 +1,14 @@
+package models
+
+import (
+	"time"
+)
+
+type product struct {
+	ID        uint   `gorm:"primaryKey"`
+	Name      string `gorm:"not null;unique;type:varchar(191)"`
+	Brand     string `gorm:"not null;unique;type:varchar(191)"`
+	UserID    uint   // Foreign Key ID from User
+	CreatedAt time.Time
+	UpdateAt  time.Time
+}	
